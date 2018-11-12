8246a700f907118d3865eb6835662b6440c89944
zhuzongyuan <zhu_zongyuan@sina.com>
2018-11-12 23:05:38 +0800

    edit test class

--- app/src/main/java/com/emap/zzy/hellogithub/newClass.java
+++ app/src/main/java/com/emap/zzy/hellogithub/newClass.java
@@ -5,4 +5,22 @@ package com.emap.zzy.hellogithub;
  */
 
 public class newClass {
+    private  int a;
+    private  String b;
+
+    public int getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
 }
